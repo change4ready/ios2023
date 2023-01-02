e6416711591466ef0b94debcc9f46b7cbfd68a74
change4ready <faithit2success@gmail.com>
2023-01-02 00:31:18 -0500

    completed swift chapter 2 types

---
+++ swift_programming/types.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+let numberOfStoplights: Int = 4
+var population: Int
+population = 5422
+let townName: String = "Knowhere"
+let elevation = 33000
+
+let townDescription = "\(townName) has a population of \(population) and \(numberOfStoplights) stoplights, with an elevation of \(elevation)."
+print(townDescription)
+
+
+
+
