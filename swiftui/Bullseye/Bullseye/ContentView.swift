e59b7bdf5daced5c0c6cfe1c2fd4d74576fd0be6
change4ready <faithit2success@gmail.com>
2023-01-10 00:17:15 -0500

    updating bullseye swiftui app

--- swiftui/Bullseye/Bullseye/ContentView.swift
+++ swiftui/Bullseye/Bullseye/ContentView.swift
@@ -11,11 +11,17 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
             Text("89")
             HStack {
                 Text("1")
                 Slider(value: .constant(50), in: 1.0...100.0)
                 Text("100")
+                 
             }
             Button(action: {}) {
                 Text("Hit Me")
