b97f4e970b6cfca061d7c002695aad9ba8562a1b
change4ready <faithit2success@gmail.com>
2023-01-18 21:34:34 -0500

    adding AsyncImage example

---
+++ AsyncImage/AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Christian Tisby on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
